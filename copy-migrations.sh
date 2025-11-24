8909194eaecf671aaf27136ec4a6cb7d3b3710a1
song <>
2025-11-24 11:44:48 +0700

    fix: migration form /prisma/migrations by default

--- copy-migrations.sh
+++ copy-migrations.sh
@@ -5,7 +5,6 @@ echo "Copying prisma migration files..."
 MIGRATIONS_TARGET=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}
 
 rm -rf "$MIGRATIONS_TARGET/migrations"
-mkdir "$MIGRATIONS_TARGET/migrations"
-cp -r ${SRCROOT}/../migrations ${MIGRATIONS_TARGET}
+cp -r ${SRCROOT}/../prisma/migrations "${MIGRATIONS_TARGET}/migrations"
 
 echo "migration files copied ✅"
